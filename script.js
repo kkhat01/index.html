1ff5728f4d81b53af50122dddd6802f9fe0f6758
Kushal Khatiwada <42869679+kkhat01@users.noreply.github.com>
2020-09-28 11:40:56 -0400

    adding script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+var modal = document.getElementById("myModal");
+    var img = document.getElementById("myImg_1");
+    var modalImg = document.getElementById("img01");
+    img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    }
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
+
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("myImg_2");
+    var modalImg = document.getElementById("img01");
+    img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    }
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
+
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("myImg_3");
+    var modalImg = document.getElementById("img01");
+    img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    }
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
+
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("myImg_4");
+    var modalImg = document.getElementById("img01");
+    img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    }
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
+
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("myImg_5");
+    var modalImg = document.getElementById("img01");
+    img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    }
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
+
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("myImg_6");
+    var modalImg = document.getElementById("img01");
+    img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    }
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
